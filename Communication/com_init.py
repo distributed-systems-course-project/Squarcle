25d8bf9e8f8a93f011910d7c6c379032061089d2
Ziizou ZIDANE <wizouzizou@yahoo.fr>
2019-03-09 16:31:28 +0200

    Communication initializer set with TCP IP parameter

--- Communication/com_init.py
+++ Communication/com_init.py
@@ -4,19 +4,25 @@ import socket 		# To manage sockets
 import netifaces 	# To check the validaty of the TCP_IP address entered
 import secrets		#  for generating cryptographically strong random numbers
 
+'''
+	This class is used to set the initial parameter of the communication between the nodes
+	It implements an empty constructor
 
+	It implements 4 getters:
+		- get_node_nbr: returns node generated number to be used as an identifier !
+		- get_node_ip: node IP address
+		- get_node_tcp_port: TCP/IP port number 
+		- get_can_play : If true => all set, node can play; If false: node can't play the game
+'''
 class Com_Init:
 
 	can_play = False;
 	node_nbr = 0;
 	node_ip = "";
-	node_port = 0;
+	node_tcp_port = 0;
 
 
-	def __init__(self, node_nbr, text):
-		self.text = text
-		self.node_nbr = node_nbr
-
+	def __init__(self):
 		'''
 		In the following section we will get the IP address of the wireless device of the node
 		'''
@@ -34,16 +40,65 @@ class Com_Init:
 		else: # No wireless device with 'w' in its name
 			self.can_play = False
 
+		if self.can_play:
+			self.node_nbr = self.geenrate_node_nbr(self.node_ip)
+
+			# Checking the availability of the port
+			node_tcp_port = 5000
+			while(not self.checkPort(node_tcp_port)):
+				node_tcp_port+=1
+
+			self.node_tcp_port = node_tcp_port
 		
+		#else => couldn't get node IP address => This node can't play the game !
 
-	def get_text(self):
-		return self.text
 
+
+
+	'''
+	Function used to generate node number
+	Input:
+		node_ip: node IP address => string
+	Return:
+		node_nbr : integer => node number
+	'''
+	def geenrate_node_nbr(self, node_ip):
+		
+		node_nbr = node_ip.split('.')[-1]
+
+		return node_nbr
+
+		
+	'''
+	Function used to check port availability
+	Input:
+		port: integer => port number to check
+	Return:
+		result: boolean => True if port is not in use; false otherwise
+	'''
+	def checkPort(self, port):
+	    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    result = False
+	    try:
+	        sock.bind(("0.0.0.0", port))
+	        result = True
+	    except:
+	        result = False
+	    sock.close()
+	    return result
+
+
+	'''
+	Getters section
+	'''
 	def get_node_nbr(self):
 		return self.node_nbr
 
 	def get_node_ip(self):
 		return self.node_ip
 
-	def get_node_port(self):
-		return self.node_port
+	def get_node_tcp_port(self):
+		return self.node_tcp_port
+
+	def get_can_play(self):
+		return self.can_play
--- main_com_tmp.py
+++ main_com_tmp.py
@@ -2,11 +2,12 @@
 from Communication import com_init
 
 
-com_init_obj = com_init.Com_Init(2, "test")
-
-print(com_init_obj.get_node_nbr())
-
-print(com_init_obj.get_text())
+com_init_obj = com_init.Com_Init()
 
 print(com_init_obj.get_node_ip())
 
+print(com_init_obj.get_node_nbr())
+
+print(com_init_obj.get_node_tcp_port())
+
+print(com_init_obj.get_can_play())
